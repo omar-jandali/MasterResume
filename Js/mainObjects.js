29d4b88957cc72ca0dcd877c115ed39680a0dff2
omar jandali <omar jandali>
2016-07-31 04:49:32 -0700

    added the initial version of the basicInfo, contactInfo, skills, and quotes objects

--- Js/mainObjects.js
+++ Js/mainObjects.js
@@ -14,3 +14,69 @@ List of objects-
  quotes
 
 */
+
+var basicInfo = {
+ "name":"Omar Jandali",
+ "role":"Front End Web Developer",
+ "message":"INSERT HERE"
+}
+
+var contactInfo = {
+ "phone":"(951) 534-3666",
+ "email":"omar@omnacore.com",
+ "address":"27806 Soller Mission Viejo, CA 92692",
+ "blog":"www.omarjandali.com/blog",
+ "social media":{
+  "twitter":"@itsmeomarj",
+  "instagram":"@itsmeomarj",
+  "youtube":"@itsmeomarj"
+ }
+}
+
+var skillsGeneral = [
+ "Social Media Specialist",
+ "Social Media Analytics",
+ "Social Media Manager",
+ "Google App's",
+ "SEO Specialist",
+ "Digital Design",
+ "Adobe Creative Suite"
+]
+
+var skillsProgramming = [
+ "HTML5",
+ "CSS3",
+ "Javascript",
+ "AJAX",
+ "jQuery",
+ "Bootstrap",
+ "Github",
+ "Git",
+ "google API's",
+ "Web Development"
+]
+
+var quotes = [
+ "If you want to succeed, you should strike out new paths, rather than travel the worn
+  paths of accepted success. - John D. Rockefeller Jr.",
+ "Don't be afraid of giving up the good for the great. - John D. Rockefeller Jr.",
+ "Innovation distinguishes between a leader and a follower - Steve Jobs",
+ "A lot of times, people don't know what they wont until you show it to them - Steve Jobs",
+ "It takes 20 years to build your reputation and 5 minutes to ruin that same reputation.
+  Think of that and you will do things very differently. - Warren Buffet",
+ "Price is what you pay for it. Value is what you get out of it. - Warren Buffet",
+ "I can either sit and watch it, or I can be a part of it - Elon Musk",
+ "There is a tremendous bias against taking risks. Everyone is trying to optimize their
+  ass-covering - Elon Musk",
+ "Don't be afraid of new arenas. - Elon Musk"
+]
+
+var projects = {
+}
+
+var experience = {
+}
+
+var education = {
+}
+
