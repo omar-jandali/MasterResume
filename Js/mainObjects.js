64289889690f6520f688bdb7e629a2fbc291f5c7
omar jandali <omar jandali>
2016-07-31 02:59:55 -0700

    added the javascript folder and mainObject file

---
+++ Js/mainObjects.js
@@ -0,0 +1,16 @@
+/*
+
+This is the mainObjects.js file which is going to contain the following information and
+scripting.
+
+List of objects-
+ contact info
+ basic info
+ skills(General)
+ skills(Programming)
+ projects
+ experiences
+ education
+ quotes
+
+*/
