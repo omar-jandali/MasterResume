b8c0c8989557d982ddf23e8483fe99d1d35283ae
omar jandali <omar jandali>
2016-08-04 05:21:45 -0700

    Added the resumeObjects.js file

---
+++ Js/resumeObjects.js
@@ -0,0 +1,32 @@
+/*This document was created by and the sole property of Omar Jandali.*/
+/*
+This is going to be the document that contains all of the objects that
+will be displayed on the resume page.
+
+The main objects include the following:
+General Information(include About Me information - possibly)
+Contact Information
+Quotes
+Projects
+Skills-general/marketing
+Skills-programming/technical
+Experience
+Education
+Contact
+Social Media
+
+There are a few additional objects that will be added:
+navigation - all the different navigation tabs
+*/
+
+var generalInfo = {}
+var contactInfo = {}
+var projects = {}
+var skillsGeneral = {}
+var skillsProgramming = {}
+var experience = {}
+var education = {}
+var quotes = []
+var socialMedia = []
+
+var navigation = []
