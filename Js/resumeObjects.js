1e967e60c3edb9f917c188a0a6b5a8555511c322
omar jandali <omar jandali>
2016-08-05 00:13:19 -0700

    Added the social media, experience, general info, contact info, skills objects

--- Js/resumeObjects.js
+++ Js/resumeObjects.js
@@ -19,14 +19,118 @@ There are a few additional objects that will be added:
 navigation - all the different navigation tabs
 */
 
-var generalInfo = {}
-var contactInfo = {}
-var projects = {}
-var skillsGeneral = {}
-var skillsProgramming = {}
-var experience = {}
-var education = {}
+var generalInfo = {
+  "name":"Omar Jandali",
+  "position":"Front End Web Developer",
+  "document":"Master Resume",
+  "location":"Mission Viejo, CA",
+  "age":"23 Years old",
+  "about":[
+    "Greatest strength - motivated, efficient",
+    "Greatest weakness - impulsive, distracted",
+    "Describe yourself - ambitious, charismatic, visionary",
+    "Dream job - Self Driving Car Software Developer at Google",
+    "Work Environment - open and free space to push creativity"
+  ]
+}
+var contactInfo = {
+  "phone":"(951) 534-3666",
+  "email":"omar@omnacore.com",
+  "location":"Mission Viejo, CA",
+  "website":"www.omarjandali.com"
+}
+var portfolio = {
+  "projects":{
+
+  }
+}
+var skillsGeneral = {
+  "marketing":[
+    "Social Analytics",
+    "Marketing Specialist",
+    "Marketing Strategy",
+    "Branding"
+  ],
+  "general":[
+    "Windows 7+",
+    "OS X leopard+",
+    "Android 4.0+",
+    "iOS 7+",
+    "Adobe Creative Suite",
+    "Google Apps"
+  ]
+}
+var skillsProgramming = {
+  "programming":[
+    "HTML5",
+    "CSS3",
+    "Javascript",
+    "C++",
+    "Java",
+    "MySQL",
+    "Php",
+    "Bootstrap",
+    "Git",
+    "GitHub",
+  ]
+}
+var experience = {
+  "jobs":[
+      {
+      "name":"OmnaCore Marketing",
+      "position":"Social Media Specialist",
+      "dates":"March 2016 - Present",
+      "location":"Mission Viejo, CA",
+      "description":""
+    },
+      {
+      "name":"Addrenal",
+      "position":"Social Media - Internship",
+      "dates":"February 2016 - May 2016",
+      "location":"Huntington Beach, CA",
+      "description":""
+    },
+      {
+      "name":"Freelance",
+      "position":"Web Designer",
+      "dates":"August 2015 - February 2016",
+      "location":"Mission Viejo, CA",
+      "description":""
+    },
+      {
+      "name":"Freelance",
+      "position":"Social Media Strategist",
+      "dates":"January 2015 - August 2015",
+      "location":"Mission Viejo, CA",
+      "description":""
+    }
+  ]
+}
+var education = {
+
+}
+var socialMedia = {
+  "icons":{
+    "instagram":""
+    "twitter":""
+    "linkedin":""
+    "youtube":""
+    "github":""
+    "blog":""
+    "email":""
+    "skype":""
+  }
+  "username":{
+    "instagram":""
+    "twitter":""
+    "linkedin":""
+    "youtube":""
+    "github":""
+    "blog":""
+    "email":""
+    "skype":""
+  }
+}
 var quotes = []
-var socialMedia = []
 
 var navigation = []
