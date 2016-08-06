fa06ed2306bd61d9cd5ac3e1d14343712f4f4c6e
omar jandali <omar jandali>
2016-08-05 22:16:29 -0700

    added the objectsHTML file which will make the the object HTML elements

---
+++ Js/objectsHTML.js
@@ -0,0 +1,16 @@
+/*This document was created by and the sole property of Omar Jandali.*/
+/*
+  This file is going to be used to created the code that will be used to
+  turn the objects into proper HTML elements that will eventually be
+  displayed in the html document.
+
+  Each of the different sections will be seperated for easy understanding
+*/
+
+/*This is going to be the header section*/
+var NameHeaderHTML = "<h1>fill</h1>";
+var ProfilePictureHTML = "<img src=\"fill\">";
+var QuoteHeaderHTML = "<h3>fill</h3>";
+var QuickInfoHeaderHTML = "<h3>fill</h3>";
+var QuickInfoHTML = "<li>fill</li>";
+
