238abeeb677d94ad292f66550c48ce491dcba2e8
omar jandali <omar jandali>
2016-08-06 03:09:24 -0700

    Added the following sections to the document including quickInfo, aboutInfo, portfolioInfo, skillsInfo, experienceInfo

--- Js/objectsHTML.js
+++ Js/objectsHTML.js
@@ -14,3 +14,35 @@ var QuoteHeaderHTML = "<h3>fill</h3>";
 var QuickInfoHeaderHTML = "<h3>fill</h3>";
 var QuickInfoHTML = "<li>fill</li>";
 
+var AboutHeaderHTML = "<h1>fill</h1>";
+var NameAboutHTML = "<h3>fill</h3>";
+var PositionAboutHTML = "<p>fill</p>";
+var LocationAboutHTML = "<p>fill</p>";
+var AgeAboutHTML = "<p>fill</p>";
+var FactsHeaderHTML= "<h3>fill</h3>":
+var FactsAboutHTML = "<p>fill</p>";
+var SummaryAboutHTML  = "<p>fill</p>";
+
+var PortfolioHeaderHTML = "<h1>fill</h1>";
+var ImagePortfolioHTML = "<img src=\"fill\">";
+var TitlePortfolioHTML = "<h3>fill</h3>";
+var AuthorPortfolioHTML = "<p>fill</p>";
+var UrlPortfolioHTML = "<p>fill</p>";
+var GithubPortfolioHTML = "<p>fill</p>";
+var DescriptionPortfolioHTML = "<p>fill</p>";
+
+var SkillsHeaderHTML = "<h1>hill</h1>";
+var GeneralTitleHTML = "<h2>fill</h2>";
+var GeneralSkillsHTML = "<li>fill</li>";
+var MarketingTitleHTML = "<h2>fill</h2>";
+var MarketingSkillsHTML = "<li>fill</li>";
+var ProgrammingTitleHTML = "<h2>fill</h2>";
+var ProgrammingSkillsHTML = "<li>fill</li>";
+
+var ExperienceHeaderHTML = "<h1>fill</h1>";
+var ImageExperienceHTML = "<img src\"fill\">";
+var NameExperienceHTML = "<h3>fill</h3>";
+var PositionExperienceHTML = "<h3>fill</h3>";
+var LocationExperienceHTML = "<h4>fill</h4>";
+var DateExperienceHTML = "<h4>fill</h4>";
+var DescriptionExperienceHTML = "<p>fill</p>";
